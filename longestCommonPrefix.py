fd59d0902fc4ac4848bbc225e466a3bae693d4bb
Mhia Mojica <mhiamojica@gmail.com>
2022-08-16 15:14:07 -0700

    index out of range when looping

--- longestCommonPrefix.py
+++ longestCommonPrefix.py
@@ -6,8 +6,10 @@ def longestCommonPrefix(strs):
 
 	for i in range(len(strs)):
 		for j in range(len(strs[i])):
-			prefix.append(j)
-		
+			if (i < len(strs)-1 and strs[i][j] == strs[i+1][j]):
+				prefix.append(strs[i][j])
+				print(prefix)
+	print(prefix)	
 
 longestCommonPrefix(inputList)
 	
