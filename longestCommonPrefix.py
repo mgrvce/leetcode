478ee44ad59e87284bb3e957db5dcc873e3989a8
Mhia Mojica <mhiamojica@gmail.com>
2022-08-16 15:07:42 -0700

    trying a stack

--- longestCommonPrefix.py
+++ longestCommonPrefix.py
@@ -1,3 +1,14 @@
-def longestCommonPrefix(strs):
+input = input("enter strings separated by space: ")
+inputList = input.split()
 
+def longestCommonPrefix(strs):
+	prefix = []
+
+	for i in range(len(strs)):
+		for j in range(len(strs[i])):
+			prefix.append(j)
+		
+
+longestCommonPrefix(inputList)
+	
 	
