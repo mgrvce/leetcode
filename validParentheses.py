c857d5560c1fa0771b2cbe32b034465994e829da
Mhia Mojica <mhiamojica@gmail.com>
2022-08-18 20:31:34 -0700

    in progress

--- validParentheses.py
+++ validParentheses.py
@@ -1,6 +1,16 @@
 input = input("enter parentheses: ")
 
 def isValidParentheses(s):
-	print(s)
+	stack = []
+
+	for p in s:
+		if (p == '(' or p == '[' or p == '{'):
+			stack.append(p)
+		else:
+			top = stack[-1]
+			if (p == ')' and top == '(' or p == ']' and top == '[' or p == '}' and top == '{'):
+				stack.pop()
+				print("invalid parentheses")
+		
 
 isValidParentheses(input)
