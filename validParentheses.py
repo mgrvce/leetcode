105f5bf8e23700f0603eced041083623161dd3f0
Mhia Mojica <mhiamojica@gmail.com>
2022-09-04 14:12:31 -0700

    Passes but slow

--- validParentheses.py
+++ validParentheses.py
@@ -2,18 +2,23 @@ input = input("enter parentheses: ")
 
 def isValidParentheses(s):
 	stack = []
+	braces = {')':'(', ']':'[','}':'{'}
 
 	for p in s:
 		if (p == '(' or p == '[' or p == '{'):
 			stack.append(p)
 		else:
-			if (not stack):
-				#print("invalid parentheses")	
+			if not stack:
+				#print("invalid")
 				return False
 			if stack:
 				top = stack[-1]
-				if p == ')' and top == '(' or p == ']' and top == '[' or p == '}' and top == '{':
-					x = stack.pop()
+				if top == braces[p]:
+					stack.pop()
+				else:
+					#print("invalid")
+					return False
+				
 	if (not stack):
 		#print("valid")
 		return True
