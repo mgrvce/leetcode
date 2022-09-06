882ecca2fdda60f7132ded047f2c7fd080ad4950
Mhia Mojica <mhiamojica@gmail.com>
2022-09-06 14:14:19 -0700

    not passing some cases

--- longestCommonPrefix.py
+++ longestCommonPrefix.py
@@ -3,13 +3,15 @@ inputList = input.split()
 
 def longestCommonPrefix(strs):
     prefix = ''
+    if len(strs) == 1:
+        return strs[0]
 
     for i in range(len(strs)-1):
         curr = ''
         for j in range(min(len(strs[i]), len(strs[i+1]))):
-            if (strs[i][j] == strs[i+1][j]):
+            if (strs[i][0] == strs[i+1][0] and strs[i][j] == strs[i+1][j]):
                 curr += strs[i][j]
-            prefix = curr
+        prefix = curr
 
     print("lcp: "+prefix)
     return prefix
