2edbcffbe885fd0b8086b73e68c208163ca91127
Mhia Mojica <mhiamojica@gmail.com>
2022-09-06 12:11:50 -0700

    progress

--- longestCommonPrefix.py
+++ longestCommonPrefix.py
@@ -2,14 +2,20 @@ input = input("enter strings separated by space: ")
 inputList = input.split()
 
 def longestCommonPrefix(strs):
-	prefix = []
+    prefix = ''
 
-	for i in range(len(strs)):
-		for j in range(len(strs[i])):
-			if (i < len(strs)-1 and strs[i][j] == strs[i+1][j]):
-				prefix.append(strs[i][j])
-				print(prefix)
-	print(prefix)	
+    for i in range(len(strs)):
+        for j in range(len(strs[i])):
+            if (i < len(strs)-1 and strs[i][j] == strs[i+1][j]):
+                print("i: "+str(i))
+                print("j: "+str(j))
+                curr = ''
+                curr += strs[i][j]
+                if curr not in prefix:
+                    prefix += curr
+                    print(prefix)
+
+    print(prefix)
 
 longestCommonPrefix(inputList)
 	
