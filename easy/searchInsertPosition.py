c8d43ef06501a43bb40a80df1ec083d6aa1f1907
Mhia Mojica <mhiamojica@gmail.com>
2022-09-09 14:23:10 -0700

    organizing files

---
+++ easy/searchInsertPosition.py
@@ -0,0 +1,8 @@
+testNums = [1, 3, 5, 6]
+
+def searchInsert(nums, target):
+    # write code here
+
+print(searchInsert(testNums, 5))
+print(searchInsert(testNums, 2))
+print(searchInsert(testNums, 7))
