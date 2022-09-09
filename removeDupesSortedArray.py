f0aea0dcd305745f3527d6e996ad1b9989530c9c
Mhia Mojica <mhiamojica@gmail.com>
2022-09-09 13:42:45 -0700

    slightly better still slow

--- removeDupesSortedArray.py
+++ removeDupesSortedArray.py
@@ -4,6 +4,28 @@ test2 = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
 def removeDupes(nums):
     if not nums:
         return 0
+    
+    uniqueElems = []
+    count = 0
+
+    for i in range(len(nums)):
+        if nums[i] not in uniqueElems:
+            uniqueElems.append(nums[i])
+            nums[i-count] = nums[i]
+        else:
+            count += 1
+
+
+    print(nums)
+
+    return len(uniqueElems)
+
+'''
+# brute force
+
+def removeDupes(nums):
+    if not nums:
+        return 0
 
     seen = []
 
@@ -15,10 +37,9 @@ def removeDupes(nums):
         nums[i] = seen[i]
 
     print(nums)
-
     return len(seen)
 
+'''
 
 print(removeDupes(test1))
-
 print(removeDupes(test2))
