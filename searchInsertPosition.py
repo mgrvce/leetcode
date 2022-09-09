d5c2068fcc497d0e364a24d21d5e7d5ad9a4a2c0
Mhia Mojica <mhiamojica@gmail.com>
2022-09-09 14:25:52 -0700

    moving files to new folder

--- searchInsertPosition.py
+++
@@ -1,8 +0,0 @@
-testNums = [1, 3, 5, 6]
-
-def searchInsert(nums, target):
-    # write code here
-
-print(searchInsert(testNums, 5))
-print(searchInsert(testNums, 2))
-print(searchInsert(testNums, 7))
