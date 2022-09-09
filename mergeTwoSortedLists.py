9b147c5aa2d4a5e5e4d3997f57ad16fbf1fd615f
Mhia Mojica <mhiamojica@gmail.com>
2022-09-08 18:28:47 -0700

    passing but slow

--- mergeTwoSortedLists.py
+++ mergeTwoSortedLists.py
@@ -5,51 +5,60 @@ class ListNode:
         self.val = val
         self.next = next
 
+def printLL(Ls):
+    r = []
+    while Ls:
+        r.append(Ls.val)
+        Ls = Ls.next
+    
+    print(r)
 
 Ls1 = ListNode(1, ListNode(2, ListNode(4)))
 Ls2 = ListNode(1, ListNode(3, ListNode(4)))
 
+Ls3 = ListNode(5)
+Ls4 = ListNode(1, ListNode(2, ListNode(4)))
+
 def mergeTwoLists(list1, list2):
     if not list1 and not list2:
         return None
-    if not list1 and list2:
+    if not list1:
         return list2
-    if not list2 and list1:
+    if not list2:
         return list1
 
     if list1.val < list2.val:
         newHead = list1
+        list1 = list1.next
     else:
         newHead = list2
+        list2 = list2.next
 
-    while list1 or list2:
-        if not list1:
-            list1 = list2
-            break
-        elif not list2:
-            list2 = list1
-            break
-        elif list1.val < list2.val:
-            temp = list1.next
-            list1.next = list2
-            list2 = list2.next
-            list1 = temp 
-        else:
-            temp = list2.next
-            list2.next = list1
+    tail = newHead
+
+    while list1 and list2:
+        if list1.val < list2.val:
+            tail.next = list1
             list1 = list1.next
-            list2 = temp
+            tail = tail.next
+        else:
+            tail.next = list2
+            list2 = list2.next
+            tail = tail.next
+    
+    if not list1:
+        tail.next = list2
+    else:
+        tail.next = list1
     
     return newHead
 
-Ls3 = mergeTwoLists(Ls1, Ls2)
+test1 = mergeTwoLists(Ls1, Ls2)
+test2 = mergeTwoLists(Ls3, Ls4)
+
+printLL(test1)
 
 '''
-while Ls3:
-    print(Ls3.val)
-    Ls3 = Ls3.next
-
-
 ls1 = list(map(int, input("enter ints for list1: ").split()))
 ls2 = list(map(int, input("enter ints for list2: ").split()))
 
