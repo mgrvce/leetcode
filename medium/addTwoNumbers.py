f031a0d96f1f7fdced7dfa43f728681c45ea1b40
Mhia Mojica <mhiamojica@gmail.com>
2022-09-12 21:53:44 -0700

    carrying one not working

--- medium/addTwoNumbers.py
+++ medium/addTwoNumbers.py
@@ -7,9 +7,10 @@ class ListNode:
 # print linked list
 def printLL(ll):
     new = []
+    curr = ll
     while curr:
-        print(ll.val)
-        curr = ll.next
+        new.append(curr.val)
+        curr = curr.next
     print(new)
 
 List1 = ListNode(2, ListNode(4, ListNode(3)))
@@ -20,23 +21,40 @@ List4 = ListNode(9, ListNode(9, ListNode(9, ListNode(9))))
 
 def addTwo(l1, l2):
 
-    head = ListNode(l1.val + l2.val)
+    val = l1.val +l2.val
+    carry = False
+    if val > 10:
+        carry = True
+        val = val - 10
+
+    head = ListNode(val)
     tail = head
     l1 = l1.next
     l2 = l2.next
 
     while l1 and l2:
-        tail.next = ListNode(l1.val + l2.val)
+        val = l1.val +l2.val
+        if carry:
+            val = val + 1
+            carry = False
+        elif val > 10:
+            carry = True
+            val = val - 10
+        tail.next = ListNode(val)
         l1 = l1.next
         l2 = l2.next
     
-    if l1.next:
+    if l1:
         tail.next = l1.next
-    elif l2.next:
+    elif l2:
         tail.next = l2.next
     else:
         return head
 
 
+printLL(List1)
+printLL(List2)
+# printLL(List3)
+# printLL(List4)
 printLL(addTwo(List1, List2))
 # printLL(addTwo(List3, List4))
