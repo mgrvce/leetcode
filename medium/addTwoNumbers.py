08360712cb785fe76b9cfeac7700546dc1611d64
Mhia Mojica <mhiamojica@gmail.com>
2022-09-12 21:28:08 -0700

    in progress

--- medium/addTwoNumbers.py
+++ medium/addTwoNumbers.py
@@ -4,6 +4,14 @@ class ListNode:
         self.val = val
         self.next = next
 
+# print linked list
+def printLL(ll):
+    new = []
+    while curr:
+        print(ll.val)
+        curr = ll.next
+    print(new)
+
 List1 = ListNode(2, ListNode(4, ListNode(3)))
 List2 = ListNode(5, ListNode(6, ListNode(4)))
 
@@ -11,7 +19,24 @@ List3 = ListNode(9, ListNode(9, ListNode(9, ListNode(9, ListNode(9, ListNode(9, 
 List4 = ListNode(9, ListNode(9, ListNode(9, ListNode(9))))
 
 def addTwo(l1, l2):
-    # code here
 
-print(addTwo(List1, List2))
-print(addTwo(List3, List4))
+    head = ListNode(l1.val + l2.val)
+    tail = head
+    l1 = l1.next
+    l2 = l2.next
+
+    while l1 and l2:
+        tail.next = ListNode(l1.val + l2.val)
+        l1 = l1.next
+        l2 = l2.next
+    
+    if l1.next:
+        tail.next = l1.next
+    elif l2.next:
+        tail.next = l2.next
+    else:
+        return head
+
+
+printLL(addTwo(List1, List2))
+# printLL(addTwo(List3, List4))
