b707768a816d9c2956de01c8fe98ed2df08e3c4e
Mhia Mojica <mhiamojica@gmail.com>
2023-04-04 00:30:47 -0700

    done

--- easy/plusOne.py
+++ easy/plusOne.py
@@ -4,33 +4,29 @@ in3 = [9]
 in4 = [3,2,9]
 
 def plusOne(digits):
-    i = -1
-    d = digits[i]
-    if d == 9:
-        while digits[i] == 9:
-            digits[i] = 0
-            print(i)
-            if len(digits) == 1 or i == 0:
-                digits.insert(0, 1)
-            i -= 1
-    else:
-        digits[i] += 1
+    num = ""
+    for i in digits:
+        num += str(i)
+    num = int(num)
+    num += 1
+    num = str(num)
+    answer = []
+    for i in num:
+        answer.append(int(i))
+    return answer
 
-    return digits
 
-    # if d == 9 and len(digits) == 1:
-    #     digits[i] = 0
-    #     digits.insert(0, 1)
-    # elif d == 9 and len(digits) > 1:
-    #     digits[-1] = 0
-    #     i = -2
+    # if d == 9:
     #     while digits[i] == 9:
     #         digits[i] = 0
+    #         print(i)
+    #         if len(digits) == 1 or i == 0:
+    #             digits.insert(0, 1)
     #         i -= 1
-    #     if i == 0:
-    #         digits.insert(0, 1)
-    # digits[i] += 1
+    # else:
+    #     digits[i] += 1
 
+    # return digits
 
 print(plusOne(in1))
 print(plusOne(in2))
